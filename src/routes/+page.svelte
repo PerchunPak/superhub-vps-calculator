ae216428a626003725419497e907475fcabd5815
PerchunPak <git@perchun.it>
2024-01-22 22:12:23 +0100

    Hardcode prices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { writable, derived } from 'svelte/store';
 
-	const cpuPrice = writable(0.88);
-	const ramPrice = writable(2.4);
-	const ssdPrice = writable(0.3);
+	const cpuPrice = 0.88;
+	const ramPrice = 2.4;
+	const ssdPrice = 0.3;
 
 	const discount = writable(10); // in percent
 
@@ -12,16 +12,16 @@
 	const ssdAmount = writable(1);
 
 	const price = derived(
-		[cpuPrice, ramPrice, ssdPrice, discount, cpuAmount, ramAmount, ssdAmount],
-		([$cpuPrice, $ramPrice, $ssdPrice, $discount, $cpuAmount, $ramAmount, $ssdAmount]) => {
-			const priceWithoutDiscount = $cpuPrice * $cpuAmount + $ramPrice * $ramAmount + $ssdPrice * $ssdAmount;
+		[discount, cpuAmount, ramAmount, ssdAmount],
+		([$discount, $cpuAmount, $ramAmount, $ssdAmount]) => {
+			const priceWithoutDiscount = cpuPrice * $cpuAmount + ramPrice * $ramAmount + ssdPrice * $ssdAmount;
 			const price = priceWithoutDiscount - (priceWithoutDiscount * $discount) / 100;
 
 			return [Number(price.toFixed(2)), Number(priceWithoutDiscount.toFixed(2))];
 		}
 	);
 
-	function toMonthPrice(price: number): number {
+	function toMonthPrice(price: number): string {
 		return (price * 30).toFixed(2);
 	}
 </script>
@@ -30,66 +30,10 @@
 	<div class="hero">
 		<div class="hero-content text-center">
 			<div class="max-w-md">
-				<h1 class="text-4xl font-bold">Prices</h1>
-
 				<label class="form-control w-full max-w-xs">
 					<div class="label">
 						<span class="label-text">CPU</span>
-					</div>
-					<input
-						type="number"
-						step="0.01"
-						bind:value={$cpuPrice}
-						class="input input-bordered w-full max-w-xs"
-					/>
-				</label>
-
-				<label class="form-control w-full max-w-xs">
-					<div class="label">
-						<span class="label-text">RAM</span>
-					</div>
-					<input
-						type="number"
-						step="0.01"
-						bind:value={$ramPrice}
-						class="input input-bordered w-full max-w-xs"
-					/>
-				</label>
-
-				<label class="form-control w-full max-w-xs">
-					<div class="label">
-						<span class="label-text">SSD</span>
-					</div>
-					<input
-						type="number"
-						step="0.01"
-						bind:value={$ssdPrice}
-						class="input input-bordered w-full max-w-xs"
-					/>
-				</label>
-
-				<label class="form-control w-full max-w-xs">
-					<div class="label">
-						<span class="label-text">Discount (in %)</span>
-					</div>
-					<input
-						type="number"
-						bind:value={$discount}
-						class="input input-bordered w-full max-w-xs"
-					/>
-				</label>
-			</div>
-		</div>
-	</div>
-
-	<div class="hero">
-		<div class="hero-content text-center">
-			<div class="max-w-md">
-				<h1 class="text-4xl font-bold">Amounts</h1>
-
-				<label class="form-control w-full max-w-xs">
-					<div class="label">
-						<span class="label-text">CPU</span>
+						<span class="label-text-alt">{cpuPrice}/1 core</span>
 					</div>
 					<input
 						type="number"
@@ -102,6 +46,7 @@
 				<label class="form-control w-full max-w-xs">
 					<div class="label">
 						<span class="label-text">RAM</span>
+						<span class="label-text-alt">{ramPrice}/1gb</span>
 					</div>
 					<input
 						type="number"
@@ -114,12 +59,25 @@
 				<label class="form-control w-full max-w-xs">
 					<div class="label">
 						<span class="label-text">SSD</span>
+						<span class="label-text-alt">{ssdPrice}/1gb</span>
 					</div>
 					<input
 						type="number"
 						step="0.5"
 						bind:value={$ssdAmount}
 						class="input input-bordered w-full max-w-xs"
+					/>
+				</label>
+
+				<label class="form-control w-full max-w-xs">
+					<div class="label">
+						<span class="label-text">Discount (in %)</span>
+					</div>
+					<input
+						type="number"
+						bind:value={$discount}
+						class="input input-bordered w-full max-w-xs"
+						disabled={true}
 					/>
 				</label>
 			</div>
